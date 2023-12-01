48c81085d0d2915850f8e8ca74d21f445412ef92
Santiago Tombolini <santitombo@gmail.com>
2023-11-30 21:29:00 -0300

    adding functionality, close to end

--- news/src/main/java/com/egg/news/controllers/NoticiaController.java
+++ news/src/main/java/com/egg/news/controllers/NoticiaController.java
@@ -1,10 +1,14 @@
 package com.egg.news.controllers;
 
+import com.egg.news.entities.Noticia;
+import com.egg.news.exceptions.MyException;
 import com.egg.news.services.NoticiaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/news")
@@ -14,8 +18,55 @@ public class NoticiaController {
 
     @GetMapping("/register")
     public String register(){
-        return "";
+        return "news_form";
+    }
+    @GetMapping("/list")
+    public String listNews(ModelMap model){
+        List<Noticia> noticias = noticiaService.listNews();
+        model.addAttribute("noticias", noticias);
+        return "news_list";
+
     }
 
+    @PostMapping("/create")
+    public String createNews(@RequestParam String title, @RequestParam String body){
+        try {
+            noticiaService.createNews(title,body);
+        } catch (MyException e) {
+            throw new RuntimeException(e);
+        }
+        return "index";
+    }
 
+    @GetMapping("/modify/{id}")
+    public String modify(@PathVariable String id, ModelMap modelo){
+        modelo.put("noticia", noticiaService.getOne(id));
+        return  "news_modify";
+    }
+    @PostMapping("/modifyNews/{id}")
+    public String modifyNews(@PathVariable String id, String title,String body, ModelMap modelo){
+        try{
+            noticiaService.modificateNews(title,body, id);
+            return "redirect:/news/list";
+        }catch (MyException ex) {
+            modelo.put("error", ex.getMessage());
+            return "news_modify";
+        }
+    }
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable String id, ModelMap modelo){
+        modelo.put("noticia", noticiaService.getOne(id));
+        return  "news_delete";
+    }
+    @PostMapping ("/deleteNews/{id}")
+    public String deleteNews(@PathVariable String id, ModelMap modelo){
+
+        try{
+            noticiaService.deleteNews(id);
+            return "redirect:/news/list";
+        }catch (MyException ex) {
+            modelo.put("error", ex.getMessage());
+            return "redirect:/news/list";
+        }
+    }
 }
